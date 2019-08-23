49bed90880134f4c07f5583210280e212b231580
Logan Senjov <logan.senjov@aginic.com>
2019-08-23 18:23:20 +1000

    Initial Commit

---
+++ test/quantum_chess/core_test.cljs
@@ -0,0 +1,10 @@
+(ns quantum-chess.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [quantum-chess.core :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
