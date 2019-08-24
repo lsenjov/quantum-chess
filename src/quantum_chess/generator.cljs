2f6f306f3e4a4b12f1507f743abd3fe51be345b7
Deepfriedice <Deepfriedice@gmail.com>
2019-08-24 15:07:13 +1000

    I am the king of trees

--- src/quantum_chess/generator.cljs
+++ src/quantum_chess/generator.cljs
@@ -1,14 +1,11 @@
 (ns ^:figwheel-hooks quantum-chess.generator)
 
 
-;(def all-chess-types (list :king :queen :rook :bishop :pawn))
-;(def chess-type-counts {:king 1 :queen 1 :rook 1 :bishop 1 :pawn 2})
 (def all-chess-types (list :P :B :N :R :Q :K))
 (def chess-type-counts {:K 1 :Q 1 :R 2 :N 2 :B 2 :P 8})
 
 
-(defn chess-piece-count
-  []
+(def chess-piece-count
   (->> chess-type-counts vals (reduce +)))
 
 (defn too-few-of
@@ -30,39 +27,13 @@
       chess-type-list)))
 
 (defn generate-histories
-  "create all possible histories"
-  [history]
-  (if (= (chess-piece-count) (count history))
-    (list history)
-    (->> all-chess-types
-      (expand-history history)
-      (map generate-histories)
-      ;(reduce concat))))
-      )))
-
-
-(defn generate-histories-from
-  "create all possible histories"
-  [history [current-chess-type & remaining-current-history :as current-history] ]
-  (if (= (chess-piece-count) (count history))
-    (list history)
-    (concat
-      (generate-histories-from
-        (conj history current-chess-type)
-        remaining-current-history)
-      (->> all-chess-types
-        (drop-while #(not= current-chess-type %))
-        (rest)
-        (expand-history history)
-        (map generate-histories)
-        (reduce concat)))))
-     
+  [root]
+  (filter #(= (count %) chess-piece-count) (tree-seq
+    #(not= chess-piece-count (count %))
+    #(expand-history % all-chess-types)
+    root
+  )))
+    
 (defn generate-first-history
   []
   (first (generate-histories [])))
-
-(defn generate-next-history
-  "Create the next history in the sequence of all possible histories"
-  [current-history]
-  (nth (generate-histories-from [] current-history) 1))
-
